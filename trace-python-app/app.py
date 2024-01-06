5a15e2d536cd2cabf6760d8afca18f079cf4a0d1
Mohammad Madanipour <97779392+mohammadll@users.noreply.github.com>
2024-01-07 00:10:41 +0330

    Update app.py

--- trace-python-app/app.py
+++ trace-python-app/app.py
@@ -1,27 +1,4 @@
 from random import randint
-from flask import Flask, request
-import logging
-
-app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-@app.route("/rolldice")
-def roll_dice():
-    player = request.args.get('player', default = None, type = str)
-    result = str(roll())
-    if player:
-        logger.warn("%s is rolling the dice: %s", player, result)
-    else:
-        logger.warn("Anonymous player is rolling the dice: %s", result)
-    return result
-
-def roll():
-    return randint(1, 6)
-
-
-
-from random import randint
 from flask import Flask
 
 from opentelemetry import trace
@@ -41,4 +18,3 @@ def roll():
         res = randint(1, 6)
         rollspan.set_attribute("roll.value", res)
         return res
-
